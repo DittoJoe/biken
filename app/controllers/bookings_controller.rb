c4b7a3f37c15b0303c8296f8cde11be825bd6261
Cristina Salazar <cristinasalazarm1@gmail.com>
2021-05-27 20:46:48 +0200

    add users controller, policy, view and display bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,7 +1,6 @@
 class BookingsController < ApplicationController
 
   def index
-    @bikes = policy_scope(Bike)
     @bookings = policy_scope(Booking)
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  resources :users do
+    resources :bookings, only: [:index]
+  end
   resources :bikes do
     resources :bookings, only: [:index, :new, :create]
   end
