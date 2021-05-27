e1856dcf58a6724a4daff5c639b4968a76ba538d
Cristina Salazar <cristinasalazarm1@gmail.com>
2021-05-27 14:27:54 +0200

    add policy to bookings

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :bikes
+  resources :bikes do
+    resources :bookings, only: [:index, :new, :create]
+  end
 end
